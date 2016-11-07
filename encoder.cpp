cce402979441669fcfbaeba5f79065caef24f359
Alex Orphan <nuk3fluk3@gmail.com>
2016-11-07 19:17:14 +0300

    1

--- encoder.cpp
+++ encoder.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <bitset>
 
 #include "encoder.hpp"
 #include "lib.hpp"
@@ -16,18 +17,23 @@ string* Encoder::getInputString(){
 	ifstream file;
 	file.open("files//input.txt", ios_base::in);
 	
-	file.seekg(0, ios_base::end);
-	int length = file.tellg();
-	file.seekg(0, ios_base::beg);
+	auto inStr = new string;
+	char tmp;
 
-	char *buffer = new char[length];
+	while(true){
+		tmp = file.get();
+		if (tmp == EOF){
+			break;
+		}
+		*inStr += tmp;
+	}
 
-	file.read(buffer, length);
 	file.close();
+	
+	cout << *inStr << endl;
 
-	delete buffer;
-	return new string(buffer, length);
-}
+	return inStr;
+	}
 
 void Encoder::createProbMap(map<char,int> &probMap, std::string &str){
 	int strLength = str.length();
@@ -57,13 +63,14 @@ vector<Character*>* Encoder::createCharVector(map<char, int> &probMap){
 }
 
 map<char, string>* Encoder::createCharMap(vector<Character*> &array){
-	log("begin charmap");
 	createBranch(0, array.size(), array);
 
 	auto charMap = new map<char, string>;
 	for(int i = 0; i < array.size(); i++){
 		charMap->insert(pair<char, string>(array[i]->self, array[i]->code));
 	}
+
+
 
 	return charMap;
 }
@@ -72,52 +79,46 @@ void Encoder::createBranch(int begin, int end, vector<Character*> &array){
 	if(end == begin){
 		return;
 	}
-	cout << "new branch from " << begin << " to " << end << endl;
 
 	int point = findBranchPoint(begin, end, array);
+
 	for(int i = begin; i < point; i++){
 		array[i]->code += "0";
 	}
 	for(int i = point; i < end; i++){
 		array[i]->code += "1";
 	}
-	cout << "branch: from " << begin << " to " << end << endl;
-	// cin.get();
-	if(begin + 1 >= point){
+
+	if(begin + 1 < point){
 		createBranch(begin, point, array);
 	}
-	if(point + 1 >= end){
+	if(point + 1 < end){
 		createBranch(point, end, array);
 	}
 }
 
 int Encoder::findBranchPoint(int begin, int end, vector<Character*> &array){
-	if(end == begin + 2){
-		return begin + 1;
+
+	vector<int> differences;
+	int tmp;
+	for(int i = begin; i < end; i++){
+		tmp = seqProbSum(begin, i, array) - seqProbSum(i, end, array);
+		if(tmp < 0){
+			tmp = -tmp;
+		}
+		differences.push_back(tmp);
+	}
+	
+	int difference = differences[0],
+			index = 0;
+	for(int i = 1; i < differences.size(); i++){
+		if(differences[i] < difference){
+			difference = differences[i];
+			index = i;
+		}
 	}
 
-	int i = begin,
-		difference = 0,
-		tmp = array[begin]->prob - seqProbSum(0, end, array);
-
-		if(tmp < 0){
-			tmp = -tmp; 
-		}
-	
-	do{
-		i++;
-		difference = tmp;
-		tmp = seqProbSum(begin, i, array) - seqProbSum(i, end, array);
-	
-		if(tmp < 0){
-			tmp = -tmp; 
-		}
-		
-		cout << "diff: " << difference << " at " << i << endl;
-	}while(tmp < difference);
-
-	cout << --i << endl;
-	return i;
+	return begin + index;
 }
 
 int Encoder::seqProbSum(int begin, int end, vector<Character*> &array){
@@ -143,9 +144,14 @@ void Encoder::encode(){
 	// 	cout << c->self << ":" << c->prob << endl;
 	// });
 	auto charMap = createCharMap(*charVector);
-	for_each(charVector->begin(), charVector->end(), [] (Character* c){
-		cout << "\'" << c->self << "\'" << ":" << c->prob << " = " << c->code << endl;
-	});
+	// for_each(charVector->begin(), charVector->end(), [] (Character* c){
+	// 	cout << "\'" << c->self << "\'" << ":" << c->prob << " = " << c->code << endl;
+	// });
+	bitset<1> zero("0");
+	bitset<1> one("1");
+	for(auto i = charMap->begin(); i != charMap->end(); i++){
+		cout << one << zero << endl;
+	}
 }
 
 Character::Character(char _self, int _prob){
